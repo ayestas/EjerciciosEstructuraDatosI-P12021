83d1703a9ee26a78a5359f9de9e870e1c052b78c
KAROL MITCHELLE AYESTAS ALEMAN <ayestaskaroll@unitec.edu>
2021-01-20 01:07:17 -0600

    Add project files.

---
+++ TrabajoClase1/main.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <conio.h>
+#include <ctime>
+#include <vector>
+
+using namespace std;
+
+int main() {
+	srand((unsigned)time(0));
+	int tamaño = 10;
+
+	int* A = nullptr;
+	A = new int[10];
+
+	vector<int> V;
+
+	for (int i = 0; i < tamaño; i++) {
+		int valorV = (rand() % 100) + 1;
+		int valorA = (rand() % 100) + 1;
+
+		V.push_back(valorV);
+		A[i] = valorA;
+	}
+
+	for (int i = 0; i < V.size(); i++) {
+		cout << "vector [" << i << "] = { " << &V[i] << ", " << V[i] << " }\n";
+	}
+
+	cout << "\n";
+
+	for (int i = 0; i < 10; i++) {
+		cout << "array [" << i << "] = { " << &A[i] << ", " << A[i] << " }\n";
+	}
+
+
+
+	_getch();
+}
