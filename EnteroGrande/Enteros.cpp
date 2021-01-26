227391dffb56ae83d629b8328d4242da7a94c3f5
KAROL MITCHELLE AYESTAS ALEMAN <ayestaskaroll@unitec.edu>
2021-01-25 23:41:24 -0600

    Tarea1

--- EnteroGrande/Enteros.cpp
+++ EnteroGrande/Enteros.cpp
@@ -1,22 +1,33 @@
 #include "Enteros.h"
 #include <iostream>
-#include <sstream>
+#include <string>
+#include <vector>
+#include <algorithm> 
+#include <functional>
 
 using namespace std;
 
 Enteros& operator+(const Enteros& a, const Enteros& b) {
 	Enteros resultado;
 	int num1, num2;
-	
+	vector<int> number1;
+	vector<int> number2;
 
 	for (int i = 0; i < a.elementos; i++) {
 		num1 = a.digitos[i];
+		number1.push_back(num1);
 	}
 
-	for (int i = 0; i < b.elementos; i++) {
-		num2 = b.digitos[i];
+	for (int j = 0; j < b.elementos; j++) {
+		num2 = b.digitos[j];
+		number2.push_back(num2);
 	}
 
+	std::transform(number1.begin(), number1.end(), number2.begin(), number1.begin(), std::plus<int>());
+
+	for (std::vector<int>::iterator it = number1.begin(); it != number1.end(); ++it) {
+		cout << *it;
+	}
 	return resultado;
 }
 
--- EnteroGrande/main.cpp
+++ EnteroGrande/main.cpp
@@ -7,8 +7,8 @@ using namespace std;
 
 int main() {
 
-	Enteros n1("20");
-	Enteros n2("15");
+	Enteros n1("1234567891234");
+	Enteros n2("1234567891234");
 	n1.Imprimir();
 	cout << " + ";
 	n2.Imprimir();
