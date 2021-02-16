0854c18f500c9ab66a7ae0da0eaf1628df49d069
KAROL MITCHELLE AYESTAS ALEMAN <ayestaskaroll@unitec.edu>
2021-02-16 14:25:37 -0600

    ExamenI

--- ExamenI/PQueue.h
+++ ExamenI/PQueue.h
@@ -41,3 +41,71 @@ private:
 
 #endif // !PQUEUE_H
 
+template<class T>
+PQueue<T>::PQueue() 
+	: primero(nullptr), ultimo(nullptr){
+}
+
+template<class T>
+void PQueue<T>::enqueue(T valor, int priority) {
+	Node<T>* nuevo = new Node<T>(valor);
+
+	if (empty()) {
+		primero = nuevo;		
+	}
+	else {
+
+		if (nuevo->data > nuevo->next->data) {
+			nuevo->next = primero;
+			primero->previous(nuevo);
+			primero = nuevo;
+		}
+		else {
+			primero->next = nuevo;
+			nuevo->previous(primero);
+			nuevo = primero;
+		}
+	}
+}
+
+template<class T>
+PQueue<T> PQueue<T>::dequeue() {
+	if (empty()) {
+		primero = nuevo;
+	}
+
+	if (primero = ultimo) {
+		delete primero;
+		primero = ultimo = nullptr;
+	}
+	else {
+		Node<T>* tmp = primero;
+
+		return tmp;
+		primero = primero->next;
+		delete tmp;
+	}
+}
+
+template<class T>
+bool PQueue<T>::isEmpty() {
+	return primero = nullptr;
+}
+
+template<class T>
+int PQueue<T>::getCountPriorities(int priority) {
+	if (empty()) {
+		primero = nuevo;
+	}
+
+}
+
+template<class T>
+void PQueue<T>::Swap(PQueue<T>)
+{
+}
+
+template<class T>
+inline void PQueue<T>::print()
+{
+}
